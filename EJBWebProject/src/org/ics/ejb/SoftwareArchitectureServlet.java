6a99f2df9769421fae39c9e113a610fce6f62a49
Valdemar99 <va7012an-s@student.lu.se>
2021-03-23 17:37:56 +0100

    Removed methods
    Removed unnecessary methods, i.e. doGet and doPost.

--- EJBWebProject/src/org/ics/ejb/SoftwareArchitectureServlet.java
+++ EJBWebProject/src/org/ics/ejb/SoftwareArchitectureServlet.java
@@ -41,20 +41,4 @@ public class SoftwareArchitectureServlet extends HttpServlet {
 		out.println("</body></html>");
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
