89941158681c9fbb26e8bb98ee73940748e1c654
Valdemar99 <va7012an-s@student.lu.se>
2021-05-01 14:13:59 +0200

    Fixed weather AJAX call

--- EJBWebProject/src/main/webapp/js/smartOffice.js
+++ EJBWebProject/src/main/webapp/js/smartOffice.js
@@ -17,6 +17,37 @@ function addRow({ officeNumber, buildingAddress, temperatureSetting, ventilation
         )
 }
 
+function ParseJsonFile(result) {
+    var lat = result.latitude;
+    var long = result.longitude;
+    var city = result.city;
+    var ipNbr = result.ip;
+    $("#city").text(city);
+    $("#ipNbr").text(ipNbr);
+    $.ajax({
+        method: "GET",
+        url: "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&units=metric" + "&APPID=e1e31265eccb81355b94fc79b19e9cf2",
+        error: ajaxWeatherReturn_Error,
+        success: ajaxWeatherReturn_Success
+    })
+    function ajaxWeatherReturn_Success(result, status, xhr) {
+        var sunrise = result.sys.sunrise;
+        var sunset = result.sys.sunset;
+        var sunriseDate = new Date(sunrise * 1000);
+        var timeStrSunrise = sunriseDate.toLocaleTimeString();
+        var sunsetDate = new Date(sunset * 1000);
+        var timeStrSunset = sunsetDate.toLocaleTimeString();
+        $("#sunrise").text("Sunrise: " + timeStrSunrise);
+        $("#sunset").text("Sunset: " + timeStrSunset);
+        $("#weather").text(result.weather[0].main);
+        $("#degree").text(result.main.temp + " \u2103");
+    }
+    function ajaxWeatherReturn_Error(result, status, xhr) {
+        alert("Error i OpenWeaterMap Ajax");
+        console.log("Ajax-api-stack:  " + status);
+    }
+}
+
 function getWeatherReport() {
     $.ajax({
         method: "GET",
@@ -53,34 +84,4 @@ function ParseJsonFileOfficeList(officeList) {
     }
 }
 
-function ParseJsonFile(result) {
-	var lat = result.latitude;
-	var long = result.longitude;
-	var city = result.city;
-	var ipNbr = result.ip
-	$("#city").text(city);
-	$("#ipNbr").text(ipNbr);
-	$.ajax({
-		method: "GET",
-		url: "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&units=metric" + "&APPID=e1e31265eccb81355b94fc79b19e9cf2",
-		error: ajaxWeatherReturn_Error,
-		success: ajaxWeatherReturn_Success
-	})
-	function ajaxWeatherReturn_Success(result, status, xhr) {
-		var sunrise = result.sys.sunrise;
-		var sunset = result.sys.sunset;
-		var sunriseDate = new Date(sunrise * 1000);
-		var timeStrSunrise = sunriseDate.toLocaleTimeString();
-		var sunsetDate = new Date(sunset * 1000);
-		var timeStrSunset = sunsetDate.toLocaleTimeString();
-		$("#sunrise").text("Sunrise: " + timeStrSunrise);
-		$("#sunset").text("Sunset: " + timeStrSunset);
-        h
-		$("#weather").text(result.weather[0].main);
-		$("#degree").text(result.main.temp + " \u2103");
-	}
-	function ajaxWeatherReturn_Error(result, status, xhr) {
-		alert("Error i OpenWeaterMap Ajax");
-		console.log("Ajax-api-stack:  " + status);
-	}
-}
+
